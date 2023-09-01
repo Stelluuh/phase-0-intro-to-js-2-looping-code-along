5ac81d1d3c746038d52bba48669898fb8ac0afeb
stella <sftamesis13@gmail.com>
2023-09-01 13:32:49 -0700

    complete all assignemnts and pass all tests

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ function writeCards(names, event) {
 }
 
 
+function countDown(anyNumber){
+    while(anyNumber > 0) {
+        console.log( anyNumber )
+        anyNumber--;
+    }
+    console.log(anyNumber)
+}
 
-// function countDown( startingNumber ) {
-//   while ( startingNumber > 0 ) {
-//     console.log( startingNumber );
-//     startingNumber -= 1;
-//   }
-//   console.log( startingNumber );
-// }
+
